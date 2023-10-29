1dad942a1b12ba69152c8b05976ced6764bd4f30
Zero <kys980501@gmail.com>
2023-10-29 16:46:43 +0900

    OblivionStatus Enum 클래스 추가

---
+++ backend/devgenie/src/main/java/com/devgenie/domain/OblivionStatus.java
@@ -0,0 +1,8 @@
+package com.devgenie.domain;
+
+public enum OblivionStatus {
+    OBLIVION_STATUS_1, //망각상태 곡선 1주기
+    OBLIVION_STATUS_2, //망각상태 곡선 2주기
+    OBLIVION_STATUS_3, //망각상태 곡선 3주기
+    OBLIVION_STATUS_4, //망각상태 곡선 4주기
+}
