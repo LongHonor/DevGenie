e478a38bb06c32fe98f4d6836b26f671585211d1
rlakim <rlagyqls2682@naver.com>
2023-11-25 02:30:57 +0900

    Add tag list
    tag chip 생성 코드 추가

--- frontend/devgenie/src/Pages/QuizListPage.js
+++ frontend/devgenie/src/Pages/QuizListPage.js
@@ -1,22 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../organisms/NavigationBar';
 
 import Quiz from '../components/QuizElementBox';
 import Feedback from '../components/AiFeedbackBox';
 import { listData } from '../questionList';
+import { taglist } from '../tagList';
 import {
   Card,
   CardActions,
   CardContent,
   CardMedia,
   Typography,
+  Autocomplete,
+  TextField,
+  Chip,
 } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
+
 const QuizListPage = () => {
+  const [tagVal, setTagVal] = useState([]);
+  const colorForTag = (tag) => {
+    switch (tag) {
+      case '데이터베이스':
+        return 'primary';
+      case '네트워크':
+        return 'warning';
+      case '운영체제':
+        return 'success';
+      case '자료구조':
+        return 'info';
+    }
+  };
+  const tagValHtml = tagVal.map((option, index) => {
+    const label = option.title || option;
+
+    return (
+      <Chip
+        key={label}
+        label={label}
+        color={colorForTag(label)}
+        sx={{ m: 1 }}
+        onDelete={() => {
+          setTagVal(tagVal.filter((entry) => entry !== option));
+        }}
+      />
+    );
+  });
+
   return (
     <div>
       <Header />
       QuizList
+      <Grid container spacing={2}>
+        <Autocomplete
+          multiple
+          limitTags={1}
+          id='multiple-limit-tags'
+          options={taglist}
+          getOptionLabel={(option) => option.title}
+          sx={{ width: 300, p: 2 }}
+          onChange={(e, newValue) => setTagVal(newValue)}
+          renderTags={() => {}}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label='tag'
+              variant='standard'
+              placeholder='Choice'
+            />
+          )}
+        />
+        <Grid>{tagValHtml}</Grid>
+      </Grid>
       <>
         <Grid
           container
---
+++ frontend/devgenie/src/tagList.js
@@ -0,0 +1,20 @@
+const taglist = [
+  {
+    id: 'NETWORK',
+    title: '네트워크',
+  },
+  {
+    id: 'OPERATING_SYSTEM',
+    title: '운영체제',
+  },
+  {
+    id: 'DATABASE',
+    title: '데이터베이스',
+  },
+  {
+    id: 'DATA_STRUCTURE',
+    title: '자료구조',
+  },
+];
+
+export { taglist };
